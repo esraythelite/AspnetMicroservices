efe2bc2ae7f9ceb8b4f0ec7098dfc6ce69956da8
Esra YAZICI <esra.yazicigut@gmail.com>
2023-09-20 17:32:29 +0300

    basket api is done

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,37 +1,48 @@
+using Basket.API.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+IConfiguration Configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddStackExchangeRedisCache(options =>
-//{
-//                                                options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
-//                                            });
+
+ConfigurationManager config = new ConfigurationManager();
+config = builder.Configuration;
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.AddSwaggerGen(c =>
                                 {
                                     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
                                 });
 
 var app = builder.Build();
-var configuration = app.Services.GetRequiredService<IConfiguration>();
-var cacheSettingsValue = configuration.GetValue<string>("CacheSettings:ConnectionString");
 
-builder.Services.AddStackExchangeRedisCache(options =>
-                                            {
-                                                options.Configuration = cacheSettingsValue;
-                                            });
+//var configuration = app.Services.GetRequiredService<IConfiguration>();
+//var cacheSettingsValue = configuration.GetValue<string>("CacheSettings:ConnectionString");
+
+//builder.Services.AddStackExchangeRedisCache(options =>
+//                                            {
+//                                                options.Configuration = cacheSettingsValue;
+//                                            });
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
 }
 
 app.UseAuthorization();
